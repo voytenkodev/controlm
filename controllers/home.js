719845705f13f0ea8aa005fc583c76adf9306f13
Bogdan <voytenko@aol.com>
2021-03-24 13:22:25 +0300

    edit sort matches and ignore node_modules

--- controllers/home.js
+++ controllers/home.js
@@ -1,12 +1,13 @@
 const MatchPost = require('../models/MatchPost.js')
 
 module.exports = async(req, res) => {
-    const matchposts = await MatchPost.find({}).populate('userid');
+    const matchposts = await MatchPost.find({}).populate('userid').sort({"date" : -1});
     var date = new Date()
     var month = date.getMonth() + 1
     var day = date.getDate()
     var year = date.getFullYear()
     var currentdate = day + '.' + month + '.' +  year
+    console.log(matchposts)
     res.render('matches',{
         matchposts, currentdate
     });
