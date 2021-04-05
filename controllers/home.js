cbe3401b034457bc498a08b0d6de5d99776bc2ab
Bogdan Voytenko <info@voytenko.dev>
2021-04-05 10:05:43 +0300

    Fix bug a lost match on day of match

--- controllers/home.js
+++ controllers/home.js
@@ -4,19 +4,17 @@ module.exports = async(req, res) => {
     const matchposts = await MatchPost.find({}).populate('userid').sort({"date" : -1});
     var date = new Date()
     var month = date.getMonth() + 1
-    var day = date.getDate()
+    var day = date.getDate() -1
     var year = date.getFullYear()
     var monthtostring = month.toString()
     if (monthtostring.length < 2){
         month = '0' + monthtostring
     }
     var daytostring = day.toString()
-    if (daytostring.length < 2){
+    if (daytostring.length != 2){
         day = '0' + daytostring
     }
-    console.log(monthtostring)
     var currentdate =  year + '.' + month + '.' + day
-    console.log(currentdate)
         res.render('matches',{
         matchposts, currentdate
     });
