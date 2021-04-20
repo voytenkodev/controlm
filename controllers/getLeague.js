8316ad3cff5fdea9bdadbcb4ed1876018a3bbf6e
Bogdan Voytenko <info@voytenko.dev>
2021-04-20 18:38:57 +0300

    edit matches page design

--- controllers/getLeague.js
+++ controllers/getLeague.js
@@ -3,10 +3,10 @@ const MatchPost = require('../models/MatchPost')
 
 module.exports = async(req, res) => {
     const matches = await (await MatchPost.find({}));
-
+    console.log(req.params)
     const league = await (await MatchPost.aggregate([
         { $match: {name: matches.league}},
-        { $project: {_id: 1}}
+        { $project: {_id: 1}}   
     ]))
 
     const matchesinleague = await (await MatchPost.aggregate([
