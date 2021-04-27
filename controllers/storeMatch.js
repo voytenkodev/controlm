015bf8791fa3a468c9e99916ee103f1a58502f3b
voytenko <voytenkodev@gmail.com>
2021-04-27 12:48:20 +0300

    smtp client

--- controllers/storeMatch.js
+++ controllers/storeMatch.js
@@ -1,9 +1,31 @@
-const MatchPost = require('../models/MatchPost')
+const SMTPClient = require('emailjs');
+
+const MatchPost = require("../models/MatchPost");
 
 module.exports = async (req, res) => {
-    await MatchPost.create({
-        ...req.body,
-        userid: req.session.userId
-    })
-    res.redirect('/matches')
-}
+  await MatchPost.create({
+    ...req.body,
+    userid: req.session.userId,
+  });
+  console.log('add match')
+  const client = new SMTPClient({
+    user: "rugby@signal-tv.com",
+    password: "hayden317",
+    host: "smtp.yandex.ru",
+    ssl: true,
+    port: 465
+  });
+  console.log('CHECK MAIL')
+  client.send(
+    {
+      text: "Add match",
+      from: "rugby@signal-tv.com",
+      to: "voytenkodev@yandex.ru",
+      subject: "test",
+    },
+    (err, message) => {
+      console.log(err || message);
+    }
+  );
+  res.redirect("/matches");
+};
