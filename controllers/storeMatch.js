5dadba8305c56e1f5317bba7607fb95f3c3fc2d8
voytenko <voytenkodev@gmail.com>
2021-04-27 13:42:02 +0300

    rm this shit

--- controllers/storeMatch.js
+++ controllers/storeMatch.js
@@ -1,5 +1,3 @@
-const SMTPClient = require('emailjs');
-
 const MatchPost = require("../models/MatchPost");
 
 module.exports = async (req, res) => {
@@ -7,27 +5,5 @@ module.exports = async (req, res) => {
     ...req.body,
     userid: req.session.userId,
   });
-  alert('add match')
-  const client = new SMTPClient({
-    user: "voytenkodev",
-    password: "ccpvkzpmbhzzoaac",
-    host: "smtp.yandex.ru",
-    ssl: true,
-  },
-  (err, message) => {
-      alert(err || message)
-  });
-  alert('OK')
-  client.send(
-    {
-      text: "Add match",
-      from: "voytenkodev@yandex.ru",
-      to: "voytenkodev@gmail.com",
-      subject: "test",
-    },
-    (err, message) => {
-      console.log(err || message);
-    }
-  );
   res.redirect("/matches");
 };
